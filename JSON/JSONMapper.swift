2e6fd45673d237eaeaf3092a4f0e69ba57609fd5
Juan Alvarez <jlalvarez18@gmail.com>
2015-03-09 11:17:35 -0400

    added JSONMapper.swift, added tweets.json for testing, updated .gitignore

---
+++ JSON/JSONMapper.swift
@@ -0,0 +1,419 @@
+//
+//  JSON.swift
+//  JSON
+//
+//  Created by Juan Alvarez on 2/11/15.
+//  Copyright (c) 2015 Alvarez Productions. All rights reserved.
+//
+
+import Foundation
+
+typealias JSONDict = [String: AnyObject]
+typealias JSONArray = [JSONDict]
+
+protocol JSONMappable {
+    init(mapper: JSONMapper<Self>)
+}
+
+enum ValueType {
+    case Bool
+    case Number
+    case String
+    case Array
+    case Dictionary
+    case Null
+}
+
+struct JSONDateFormatter {
+    private static var formatters: [String: NSDateFormatter] = [:]
+    
+    static func registerDateFormatter(formatter: NSDateFormatter, withKey key: String) {
+        formatters[key] = formatter
+    }
+    
+    static func dateFormatterWith(key: String) -> NSDateFormatter? {
+        return formatters[key]
+    }
+}
+
+class JSONMapper <N: JSONMappable> {
+    
+    var jsonObject: JSONDict!
+    
+    init() {}
+    
+    func map(json: JSONDict) -> N {
+        jsonObject = json
+        
+        let object = N(mapper: self)
+        
+        return object
+    }
+    
+    func map(jsonObjects: JSONArray) -> [N] {
+        let results = jsonObjects.map({ (json: JSONDict) -> N in
+            return self.map(json)
+        })
+        
+        return results
+    }
+    
+    func valueTypeFor(key: String) -> ValueType {
+        if let value: AnyObject = valueFor(key) {
+            switch value {
+            case let number as NSNumber:
+                if number.isBool {
+                    return .Bool
+                } else {
+                    return .Number
+                }
+            case let string as String:
+                return .String
+            case let array as [AnyObject]:
+                return .Array
+            case let dict as [String: AnyObject]:
+                return .Dictionary
+            default:
+                return .Null
+            }
+        }
+        
+        return .Null
+    }
+    
+    subscript(key: String) -> AnyObject? {
+        return valueFor(key)
+    }
+}
+
+private extension JSONMapper {
+    
+    func valueFor(key: String) -> AnyObject? {
+        return valueFor(key.componentsSeparatedByString("."), dictionary: jsonObject)
+    }
+    
+    func valueFor(keys: [String], dictionary: JSONDict) -> AnyObject? {
+        if keys.isEmpty {
+            return nil
+        }
+        
+        if let object: AnyObject = dictionary[keys.first!] {
+            switch object {
+            case is NSNull:
+                return nil
+            case let dict as JSONDict where keys.count > 1:
+                let tail = Array(keys[1..<keys.count])
+                
+                return valueFor(tail, dictionary: dict)
+            default:
+                return object
+            }
+        }
+        
+        return nil
+    }
+}
+
+// MARK: NSDate
+
+extension JSONMapper {
+
+    typealias DateTransformerFromInt = (value: Int) -> NSDate?
+    typealias DateTransformerFromString = (value: String) -> NSDate?
+    
+    func dateFromIntFor(key: String, transform: DateTransformerFromInt) -> NSDate? {
+        if let value = intFor(key) {
+            return transform(value: value)
+        }
+        
+        return nil
+    }
+    
+    func dateFromStringFor(key: String, transform: DateTransformerFromString) -> NSDate? {
+        if let value = stringFor(key) {
+            return transform(value: value)
+        }
+        
+        return nil
+    }
+}
+
+// MARK: URL
+
+extension JSONMapper {
+    
+    func urlFrom(key: String) -> NSURL? {
+        if let value = stringFor(key) {
+            return NSURL(string: value)
+        }
+        
+        return nil
+    }
+}
+
+// MARK: String
+
+extension JSONMapper {
+    
+    func stringFor(key: String) -> String? {
+        return valueFor(key) as? String
+    }
+    
+    func stringValueFor(key: String) -> String {
+        if let value = stringFor(key) {
+            return value
+        }
+        
+        return ""
+    }
+}
+
+// MARK: Int
+
+extension JSONMapper {
+    
+    func intFor(key: String) -> Int? {
+        return valueFor(key) as? Int
+    }
+    
+    func intValueFor(key: String) -> Int {
+        if let value = intFor(key) {
+            return value
+        }
+        
+        return 0
+    }
+}
+
+// MARK: Bool
+
+extension JSONMapper {
+    
+    func boolFor(key: String) -> Bool? {
+        return valueFor(key) as? Bool
+    }
+    
+    func boolValueFor(key: String) -> Bool {
+        if let value = boolFor(key) {
+            return value
+        }
+        
+        return false
+    }
+}
+
+// MARK: Double
+
+extension JSONMapper {
+    
+    func doubleFor(key: String) -> Double? {
+        return valueFor(key) as? Double
+    }
+    
+    func doubleValueFor(key: String) -> Double {
+        if let value = doubleFor(key) {
+            return value
+        }
+        
+        return 0.0
+    }
+}
+
+// MARK: Float
+
+extension JSONMapper {
+    
+    func floatFor(key: String) -> Float? {
+        return valueFor(key) as? Float
+    }
+    
+    func floatValueFor(key: String) -> Float {
+        if let value = floatFor(key) {
+            return value
+        }
+        
+        return 0.0
+    }
+}
+
+// MARK: Array
+
+extension JSONMapper {
+    
+    func arrayFor(key: String) -> Array<AnyObject>? {
+        return valueFor(key) as? Array<AnyObject>
+    }
+    
+    func arrayValueFor(key: String) -> Array<AnyObject> {
+        if let value = arrayFor(key) {
+            return value
+        }
+        
+        return []
+    }
+}
+
+// MARK: Dictionary
+
+extension JSONMapper {
+    
+    func dictionaryFor(key: String) -> JSONDict? {
+        return valueFor(key) as? JSONDict
+    }
+    
+    func dictionaryValueFor(key: String) -> JSONDict {
+        if let value = dictionaryFor(key) {
+            return value
+        }
+        
+        return [:]
+    }
+}
+
+// MARK: Object: JSONMappable
+
+extension JSONMapper {
+    
+    func objectArrayFor<T: JSONMappable>(key: String) -> [T]? {
+        if let arrayValues = valueFor(key) as? JSONArray {
+            let mapper = JSONMapper<T>()
+            
+            let newValues = arrayValues.map({ (dict: JSONDict) -> T in
+                return mapper.map(dict)
+            })
+            
+            return newValues
+        }
+        
+        return nil
+    }
+    
+    func objectArrayValueFor<T: JSONMappable>(key: String) -> [T] {
+        if let values: [T] = objectArrayFor(key) {
+            return values
+        }
+        
+        return []
+    }
+    
+    func objectFor<T: JSONMappable>(key: String) -> T? {
+        if let dict = dictionaryFor(key) {
+            let mapper = JSONMapper<T>()
+            
+            let object = mapper.map(dict)
+            
+            return object
+        }
+        
+        return nil
+    }
+}
+
+// MARK: Transforms
+
+extension String {
+    func transform<T>(block: (value: String) -> T?) -> T? {
+        return block(value: self)
+    }
+}
+
+extension Int {
+    func transform<T>(block: (value: Int) -> T?) -> T? {
+        return block(value: self)
+    }
+}
+
+extension Double {
+    func transform<T>(block: (value: Double) -> T?) -> T? {
+        return block(value: self)
+    }
+}
+
+extension Float {
+    func transform<T>(block: (value: Float) -> T?) -> T? {
+        return block(value: self)
+    }
+}
+
+// MARK: - NSNumber: Comparable
+
+private let trueNumber = NSNumber(bool: true)
+private let falseNumber = NSNumber(bool: false)
+private let trueObjCType = String.fromCString(trueNumber.objCType)
+private let falseObjCType = String.fromCString(falseNumber.objCType)
+
+extension NSNumber: Comparable {
+    var isBool:Bool {
+        get {
+            let objCType = String.fromCString(self.objCType)
+            if (self.compare(trueNumber) == NSComparisonResult.OrderedSame && objCType == trueObjCType) || (self.compare(falseNumber) == NSComparisonResult.OrderedSame && objCType == falseObjCType){
+                return true
+            } else {
+                return false
+            }
+        }
+    }
+}
+
+public func ==(lhs: NSNumber, rhs: NSNumber) -> Bool {
+    switch (lhs.isBool, rhs.isBool) {
+    case (false, true):
+        return false
+    case (true, false):
+        return false
+    default:
+        return lhs.compare(rhs) == NSComparisonResult.OrderedSame
+    }
+}
+
+public func !=(lhs: NSNumber, rhs: NSNumber) -> Bool {
+    return !(lhs == rhs)
+}
+
+public func <(lhs: NSNumber, rhs: NSNumber) -> Bool {
+    
+    switch (lhs.isBool, rhs.isBool) {
+    case (false, true):
+        return false
+    case (true, false):
+        return false
+    default:
+        return lhs.compare(rhs) == NSComparisonResult.OrderedAscending
+    }
+}
+
+public func >(lhs: NSNumber, rhs: NSNumber) -> Bool {
+    
+    switch (lhs.isBool, rhs.isBool) {
+    case (false, true):
+        return false
+    case (true, false):
+        return false
+    default:
+        return lhs.compare(rhs) == NSComparisonResult.OrderedDescending
+    }
+}
+
+public func <=(lhs: NSNumber, rhs: NSNumber) -> Bool {
+    
+    switch (lhs.isBool, rhs.isBool) {
+    case (false, true):
+        return false
+    case (true, false):
+        return false
+    default:
+        return lhs.compare(rhs) != NSComparisonResult.OrderedDescending
+    }
+}
+
+public func >=(lhs: NSNumber, rhs: NSNumber) -> Bool {
+    
+    switch (lhs.isBool, rhs.isBool) {
+    case (false, true):
+        return false
+    case (true, false):
+        return false
+    default:
+        return lhs.compare(rhs) != NSComparisonResult.OrderedAscending
+    }
+}
--- JSON/ViewController.swift
+++ JSON/ViewController.swift
@@ -20,6 +20,131 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.locale = NSLocale(localeIdentifier: "en_US_POSIX")
+        dateFormatter.dateFormat = "EEE MMM dd HH:mm:ss Z yyyy"
+        
+        JSONDateFormatter.registerDateFormatter(dateFormatter, withKey: "TweetDateFormatter")
+        
+        let jsonURL = NSBundle.mainBundle().URLForResource("tweets", withExtension: "json")
+        let jsonData = NSData(contentsOfURL: jsonURL!)
+        
+        var error: NSError?
+        if let json = NSJSONSerialization.JSONObjectWithData(jsonData!, options: NSJSONReadingOptions.allZeros, error: &error) as? JSONArray {
+            let mapper = JSONMapper<Tweet>()
+            
+            let tweets = mapper.map(json)
+            
+            println(tweets)
+        } else {
+            println(error)
+        }
+    }
+}
 
+struct Tweet: JSONMappable {
+    let user: User
+    let text: String
+    let screenName: String
+    let createdAt: NSDate?
+    let favorited: Bool
+
+    init(mapper: JSONMapper<Tweet>) {
+        user = mapper.objectFor("user")!
+        screenName = mapper.stringValueFor("user.screen_name")
+        text = mapper.stringValueFor("text")
+        favorited = mapper.boolValueFor("favorited")
+        
+        let dateFormatter = JSONDateFormatter.dateFormatterWith("TweetDateFormatter")!
+        
+        createdAt = mapper.dateFromStringFor("created_at", transform: { (value) -> NSDate? in
+            return dateFormatter.dateFromString(value)
+        })
+    }
+}
+
+struct User: JSONMappable {
+    let name: String
+    let idString: String
+    let id: Int
+    let createdAt: NSDate?
+    let urls: [URL]?
+    let defaultProfile: Bool
+    let followersCount: Int
+    let backgroundColor: UIColor?
+    
+    init(mapper: JSONMapper<User>) {
+        name = mapper.stringValueFor("name")
+        idString = mapper.stringValueFor("id_str")
+        id = mapper.intValueFor("id")
+        urls = mapper.objectArrayValueFor("entities.description.urls")
+        defaultProfile = mapper.boolValueFor("default_profile")
+        followersCount = mapper.intValueFor("followers_count")
+        
+        backgroundColor = mapper.stringFor("profile_background_color")?.transform({ (value) -> UIColor? in
+            return UIColor.fromHex(value)
+        })
+        
+        let dateFormatter = JSONDateFormatter.dateFormatterWith("TweetDateFormatter")!
+        
+        createdAt = mapper.dateFromStringFor("created_at", transform: { (value) -> NSDate? in
+            return dateFormatter.dateFromString(value)
+        })
+    }
+}
+
+struct URL: JSONMappable {
+    let displayURL: NSURL?
+    let expandedURL: NSURL?
+    let url: NSURL?
+    
+    init(mapper: JSONMapper<URL>) {
+        displayURL = mapper.urlFrom("display_url")
+        expandedURL = mapper.urlFrom("expanded_url")
+        url = mapper.urlFrom("url")
+    }
+}
+
+extension UIColor {
+    public class func fromRGB(r: Int, _ g: Int, _ b: Int) -> UIColor {
+        return UIColor(red: CGFloat(r)/255.0, green: CGFloat(g)/255.0, blue: CGFloat(b)/255.0, alpha: 1)
+    }
+    
+    public class func fromHex(hex: String) -> UIColor {
+        var str = hex.uppercaseString
+        
+        if str.hasPrefix("#") {
+            str = str.substring(1..<count(str))!
+        }
+        
+        if count(str) != 6 {
+            fatalError("hex must have 6 charachters: \(hex)")
+        }
+        
+        var r: UInt32 = 0
+        var g: UInt32 = 0
+        var b: UInt32 = 0
+        
+        NSScanner(string: str.substring(0...1)!).scanHexInt(&r)
+        NSScanner(string: str.substring(2...3)!).scanHexInt(&g)
+        NSScanner(string: str.substring(4...5)!).scanHexInt(&b)
+        
+        return fromRGB(Int(r), Int(g), Int(b))
+    }
+}
+
+extension String {
+    func substring(range: Range<Int>) -> String? {
+        if range.startIndex < 0 || range.endIndex > count(self) {
+            return nil
+        }
+        
+        let range = Range(start: advance(startIndex, range.startIndex), end: advance(startIndex, range.endIndex))
+        
+        return self[range]
+    }
 }
 
